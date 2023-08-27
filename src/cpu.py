b2682b6487064c7fef7b47ba960df4b6d7bae909
Vladimir Leshkevich <asteropheum@gmail.com>
2023-08-28 00:21:04 +0700

    Update.

--- src/cpu.py
+++ src/cpu.py
@@ -9,6 +9,25 @@ class CPU:
         self.status:          ctypes.c_uint8  = ctypes.c_uint8(0)
         self.program_counter: ctypes.c_uint16 = ctypes.c_uint16(0)
 
+    def lda(self, value: int) -> None:
+        self.register_a.value = value
+        self.update_zero_and_negative_flags(self.register_a)
+
+    def tax(self) -> None:
+        self.register_x = self.register_a
+        self.update_zero_and_negative_flags(self.register_x)
+
+    def update_zero_and_negative_flags(self, result: ctypes.c_uint8):
+        if result.value == 0:
+            self.status.value |= 0b0000_0010
+        else:
+            self.status.value &= 0b1111_1101
+
+        if result.value & 0b1000_0000 != 0:
+            self.status.value |= 0b1000_0000
+        else:
+            self.status.value &= 0b0111_1111
+
     def interpret(self, program: ctypes.Array[ctypes.c_uint8]) -> None:
         """
         Interprets the given program.
@@ -30,29 +49,10 @@ class CPU:
                 case 0xA9:
                     param = program[self.program_counter.value]
                     self.program_counter.value += 1
-                    self.register_a.value = param
 
-                    if self.register_a.value == 0:
-                        self.status.value |= 0b0000_0010
-                    else:
-                        self.status.value &= 0b1111_1101
-
-                    if self.register_a.value & 0b1000_0000 != 0:
-                        self.status.value |= 0b1000_0000
-                    else:
-                        self.status.value &= 0b0111_1111
+                    self.lda(param)
                 case 0xAA:
-                    self.register_x = self.register_a
-
-                    if self.register_x.value == 0:
-                        self.status.value |= 0b0000_0010
-                    else:
-                        self.status.value &= 0b1111_1101
-
-                    if self.register_x.value & 0b1000_0000 != 0:
-                        self.status.value |= 0b1000_0000
-                    else:
-                        self.status.value &= 0b0111_1111
+                    self.tax()
                 case 0x00:
                     break
                 case _: pass
