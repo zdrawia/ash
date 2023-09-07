d462d284b60ef18735a57ee37d3eb732f1611676
Vladimir Leshkevich <asteropheum@gmail.com>
2023-09-07 20:38:23 +0700

    Update.

--- src/cpu.py
+++ src/cpu.py
@@ -1,54 +1,53 @@
-import ctypes
 from .log import logger
 
 
 class CPU:
     def __init__(self):
-        self.register_a:      ctypes.c_uint8  = ctypes.c_uint8(0)
-        self.register_x:      ctypes.c_uint8  = ctypes.c_uint8(0)
-        self.status:          ctypes.c_uint8  = ctypes.c_uint8(0)
-        self.program_counter: ctypes.c_uint16 = ctypes.c_uint16(0)
+        self.register_a:      int = 0
+        self.register_x:      int = 0
+        self.status:          int = 0
+        self.program_counter: int = 0
 
     def lda(self, value: int) -> None:
-        self.register_a.value = value
+        self.register_a = value
         self.update_zero_and_negative_flags(self.register_a)
 
     def tax(self) -> None:
         self.register_x = self.register_a
         self.update_zero_and_negative_flags(self.register_x)
 
-    def update_zero_and_negative_flags(self, result: ctypes.c_uint8):
-        if result.value == 0:
-            self.status.value |= 0b0000_0010
+    def update_zero_and_negative_flags(self, result: int):
+        if result == 0:
+            self.status |= 0b0000_0010
         else:
-            self.status.value &= 0b1111_1101
+            self.status &= 0b1111_1101
 
-        if result.value & 0b1000_0000 != 0:
-            self.status.value |= 0b1000_0000
+        if result & 0b1000_0000 != 0:
+            self.status |= 0b1000_0000
         else:
-            self.status.value &= 0b0111_1111
+            self.status &= 0b0111_1111
 
-    def interpret(self, program: ctypes.Array[ctypes.c_uint8]) -> None:
+    def interpret(self, program: list[int]) -> None:
         """
         Interprets the given program.
 
         Args:
-            program (ctypes.Array[ctypes.c_uint8]): The program to be interpreted.
+            program (list[int]): The program to be interpreted.
 
         Returns:
             None: This function does not return anything.
         """
-        self.program_counter.value = 0
+        self.program_counter = 0
 
         while True:
-            opcode = program[self.program_counter.value]
+            opcode = program[self.program_counter]
             logger.debug(f"opcode encountered: {hex(opcode)}")
-            self.program_counter.value += 1
+            self.program_counter += 1
 
             match opcode:
                 case 0xA9:
-                    param = program[self.program_counter.value]
-                    self.program_counter.value += 1
+                    param = program[self.program_counter]
+                    self.program_counter += 1
 
                     self.lda(param)
                 case 0xAA:
--- test/test_cpu.py
+++ test/test_cpu.py
@@ -1,23 +1,22 @@
 import unittest
 from src.cpu import CPU
-from src.utils import to_c_array_uint8
 
 
 class TestCPU(unittest.TestCase):
     def test_0xa9_lda_immediate_load_data(self):
         cpu = CPU()
-        cpu.interpret(to_c_array_uint8([0xA9, 0x05, 0x00]))
-        self.assertEqual(cpu.register_a.value, 0x05)
-        self.assertEqual(cpu.status.value & 0b0000_0010, 0b00)
-        self.assertEqual(cpu.status.value & 0b1000_0000, 0)
+        cpu.interpret([0xA9, 0x05, 0x00])
+        self.assertEqual(cpu.register_a, 0x05)
+        self.assertEqual(cpu.status & 0b0000_0010, 0b00)
+        self.assertEqual(cpu.status & 0b1000_0000, 0)
 
     def test_0xa9_lda_zero_flag(self):
         cpu = CPU()
-        cpu.interpret(to_c_array_uint8([0xA9, 0x00, 0x00]))
-        self.assertEqual(cpu.status.value & 0b0000_0010, 0b10)
+        cpu.interpret([0xA9, 0x00, 0x00])
+        self.assertEqual(cpu.status & 0b0000_0010, 0b10)
 
     def test_0xaa_tax_move_a_to_x(self):
         cpu = CPU()
-        cpu.register_a.value = 10
-        cpu.interpret(to_c_array_uint8([0xAA, 0x00]))
-        self.assertEqual(cpu.register_x.value, 10)
+        cpu.register_a = 10
+        cpu.interpret([0xAA, 0x00])
+        self.assertEqual(cpu.register_x, 10)
